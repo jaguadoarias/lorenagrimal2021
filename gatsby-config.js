c2187f9606a6f43feb6eae704d19734b96132164
Javier Aguado <jaguadoarias@gmail.com>
2021-04-30 00:07:55 +0200

    Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Lorena Grimal',
+    siteUrl: 'https://www.lorenagrimal.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'lorenagrimal',
+        short_name: 'lorenagrimal',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
