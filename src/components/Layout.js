c28404c7a8f89a2944a2f223ee9252e0661b5649
Javier Aguado <jaguadoarias@gmail.com>
2021-11-05 18:39:34 +0100

    Adding facebook pixel bottom page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -62,23 +62,6 @@ export default ({ children, meta, title }) => {
               <link rel="preconnect" href="https://fonts.gstatic.com" />              
               <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Josefin+Sans:wght@300;400;700&display=swap" rel="stylesheet"></link>
               <script async src="https://js.convertflow.co/production/websites/26159.js"></script>
-              <script
-                dangerouslySetInnerHTML={{ __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '427346455568715');
-                fbq('track', 'PageView');
-                `}}
-              />
-              <noscript>{`<img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=427346455568715&ev=PageView&noscript=1"
-              />`}</noscript>
             </Helmet>
 
             <Meta
@@ -97,6 +80,23 @@ export default ({ children, meta, title }) => {
             {children}
             <div className="cf-26159-area-79633"></div>    
             <Footer />
+            <script
+                dangerouslySetInnerHTML={{ __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '427346455568715');
+                fbq('track', 'PageView');
+                `}}
+              />
+              <noscript>{`<img height="1" width="1" style="display:none"
+              src="https://www.facebook.com/tr?id=427346455568715&ev=PageView&noscript=1"
+              />`}</noscript>
           </>
         )
       }}
