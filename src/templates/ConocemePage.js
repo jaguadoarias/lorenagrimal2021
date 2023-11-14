e5255fb222412e34a9c55d8caf9147a1c898368e
Javier Aguado <jaguadoarias@gmail.com>
2023-11-14 21:29:58 +0100

    New form

--- src/templates/ConocemePage.js
+++ src/templates/ConocemePage.js
@@ -73,7 +73,23 @@ export const ConocemePageTemplate = ({
                    <FormSimpleAjax />
                  </div>
                  <div style={{ display: 'none' }}>
-                   <FormSimple />
+                   <form name="New form" method="POST" data-netlify="true">
+                     <p>
+                       <label htmlFor="name">Name</label>
+                       <input type="text" id="name" name="name" />
+                     </p>
+                     <p>
+                       <label htmlFor="email">Email</label>
+                       <input type="text" id="email" name="email" />
+                     </p>
+                     <p>
+                       <label htmlFor="message">Message</label>
+                       <textarea id="message" name="message"></textarea>
+                     </p>
+                     <p>
+                       <button type="submit">Send</button>
+                     </p>
+                   </form>
                  </div>
 
                  <figure className="Contact--image">
